d1b78c81d47038f014e0943f61e545f34e5f3eba
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 12:09:01 -0600

    add exercise1

--- Conditionals.cs
+++ Conditionals.cs
@@ -4,6 +4,18 @@ namespace ControlFlow
 {
     class Conditionals
     {
+        public void Exercise1()
+        {
+            Console.WriteLine("Enter a number between 1 and 10: ");
+            var input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+
+            if (number >= 1 && number <= 10)
+                Console.WriteLine("Valid number");
+            else 
+                Console.WriteLine("Invalid number");
+        }
+
         static void Main(string[] args)
         {
 
