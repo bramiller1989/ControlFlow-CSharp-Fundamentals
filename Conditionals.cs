b623e08f0823d5f792444a4ad318285fe3cc80da
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 12:18:20 -0600

    add exercise2

--- Conditionals.cs
+++ Conditionals.cs
@@ -16,6 +16,18 @@ namespace ControlFlow
                 Console.WriteLine("Invalid number");
         }
 
+        public void Exercise2()
+        {
+            Console.WriteLine("Enter a number: ");
+            var number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+
+            var max = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("Max is " + max);
+        }
+
         static void Main(string[] args)
         {
 
